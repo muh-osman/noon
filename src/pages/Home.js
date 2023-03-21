e782dcfa65ea65244163e4670b6437691b78edf2
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-03-21 16:59:51 +0300

    Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -133,7 +133,7 @@ function Home() {
           </Grid>
 
           <div style={{textAlign: "center", fontSize: "14px", marginTop: "24px",}}>
-            <p style={{ display: "flex", alignItems: "center", justifyContent:"center",}}>Made with<span style={{display: "flex", alignItems: "center"}}>&nbsp;<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color: "red", fontSize: "14px",}}><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>&nbsp;</span>by&nbsp;<a href="https://github.com/muh-osman" target="_blank" rel="noopener noreferrer">Muh-Osman</a></p>
+            <p style={{color:"#f1f1f1", display: "flex", alignItems: "center", justifyContent:"center",}}>Made with<span style={{display: "flex", alignItems: "center"}}>&nbsp;<svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color: "red", fontSize: "14px",}}><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>&nbsp;</span>by&nbsp;<a href="https://github.com/muh-osman" target="_blank" rel="noopener noreferrer">Muh-Osman</a></p>
           </div>
         </Box>
     </>
